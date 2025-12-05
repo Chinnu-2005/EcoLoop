9d6cadc2248d3e464fe3c4cff0adb582545fdb46
Chinnu-2005 <gkrvkoushik@gmail.com>
2025-12-05 18:25:35 +0530

    edit

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,24 @@ const express=require('express');
 const cors=require('cors');
 const cookie_parser=require('cookie-parser');
 
-
 const app=express();
 
-
+// Middleware
 app.use(express.json());
 app.use(cors());
-
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.use(cookie_parser());
+
+// Routes
+const userRoutes = require('./routes/user.routes');
+const wasteRoutes = require('./routes/waste.routes');
+const recyclingRoutes = require('./routes/recycling.routes');
+const adminRoutes = require('./routes/admin.routes');
+
+app.use('/api/users', userRoutes);
+app.use('/api/waste', wasteRoutes);
+app.use('/api/recycling', recyclingRoutes);
+app.use('/api/admin', adminRoutes);
 
 module.exports=app;
